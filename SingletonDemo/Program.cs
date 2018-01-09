8e99c3ddbe1585869e8c65c8da5eade45f95ce1b
AUTH\tomarv <vijetatomar22@gmail.com>
2018-01-09 12:41:59 +0530

    first commit

--- SingletonDemo/Program.cs
+++ SingletonDemo/Program.cs
@@ -6,7 +6,13 @@ namespace SingletonDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Singleton s1 = Singleton.ReturnInstance;
+            s1.DisplayFunc("This is first msg");
+
+            Singleton s2 = Singleton.ReturnInstance;
+            s2.DisplayFunc("This is Second msg");
+            Console.ReadLine();
+
         }
     }
 }
---
+++ SingletonDemo/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDemo
+{
+    public sealed class Singleton
+    {
+        private static int count=0;
+        private Singleton()
+        {
+            count++;
+            Console.WriteLine(count);
+        }
+        private static Singleton instance = null;
+        public static Singleton ReturnInstance
+        {
+            get {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+
+        public void DisplayFunc(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
