e327de1ca225f0d9e9afdcf458f667b9313ea040
AUTH\tomarv <vijetatomar22@gmail.com>
2018-01-10 11:37:22 +0530

    add singleton with multithreading & lazy loading

---
+++ SingletonMultithreading/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SingletonMultithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(() => FirstFunc(),
+                            () => SecondFunc());
+
+            Console.ReadLine();
+        }
+
+        private static void SecondFunc()
+        {
+            Singleton s2 = Singleton.ReturnInstance;
+            s2.DisplayFunc("This is Second msg");
+        }
+
+        private static void FirstFunc()
+        {
+            Singleton s1 = Singleton.ReturnInstance;
+            s1.DisplayFunc("This is first msg");
+        }
+    }
+}
---
+++ SingletonMultithreading/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonMultithreading
+{
+    public sealed class Singleton
+    {
+        private static int count = 0;
+        private Singleton()
+        {
+            count++;
+            Console.WriteLine(count);
+        }
+       // private static readonly object objLock = new object();
+        private static readonly Lazy<Singleton> instance = new Lazy<Singleton>(()=>new Singleton());
+
+        public static Singleton ReturnInstance
+        {
+            get
+            {
+                //if (instance == null)
+                //{
+                //    lock (objLock)
+                //    {
+                //        if (instance == null)
+                //            instance = new Singleton();
+                //    }
+                //}
+                return instance.Value;
+            }
+        }
+
+        public void DisplayFunc(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
